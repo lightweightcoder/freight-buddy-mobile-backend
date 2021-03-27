0aabbca365a254fab75c5161b0c83fbb03491e21
aljt <alvinljt@gmail.com>
2021-03-27 16:18:10 +0800

    added code to test axios requests

--- routes.mjs
+++ routes.mjs
@@ -2,9 +2,12 @@ import db from './models/index.mjs';
 // import controllers here
 
 export default function bindRoutes(app) {
-
   // initialize the controller functions here
   // pass in the db for all callbacks
 
   // define route matchers here using app
+  app.get('/home', (request, response) => {
+    console.log('request came in');
+    response.send({ test: 'test' });
+  });
 }
