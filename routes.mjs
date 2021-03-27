f9ac29e218f9802dafef74e56ec11fa0ada8ce14
aljt <alvinljt@gmail.com>
2021-03-28 00:21:02 +0800

    axios tests succeeded, deleted test code

--- routes.mjs
+++ routes.mjs
@@ -6,15 +6,4 @@ export default function bindRoutes(app) {
   // pass in the db for all callbacks
 
   // define route matchers here using app
-  app.get('/home', (request, response) => {
-    console.log('request came in');
-    response.send({ test: 'test' });
-  });
-
-  app.post('/home', (request, response) => {
-    console.log('post request to home came in');
-    console.log(request.body, 'request.body');
-
-    response.send({ test: 'post to home route test success' });
-  });
 }
